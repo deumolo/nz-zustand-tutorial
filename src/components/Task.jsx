06ea2bb8a6fb180cc758070c73561a41434873c3
Moreno, Daniel <DANIEL_MORENO3@homedepot.com>
2023-09-19 14:34:14 -0600

    Installation and use of classnames utility

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,15 @@
+import classNames from 'classnames';
 import './Task.css'
 import React from 'react';
 
-const STATUS = "PLANNED"
+const STATUS = 'PLANNED';
 
 function Task({ title }) {
   return <div className='task'>
     <div>{title}</div>
     <div className='bottomWrapper'>
       <div></div>
-      <div className='status'>{ STATUS }</div>
+      <div className={classNames('status', STATUS)}> Status </div>
     </div>
   </div>;
 }
